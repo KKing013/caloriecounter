5e1d894604eba0edbf2ab371abb0b7fc78d7c3b4
Kevin King <kjking013@live.com>
2021-10-04 13:57:04 +0200

    Added get items Added add item to UI Added add item to data stucture

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 const ItemCtrl = (function () {
 
     const Item = function (id, name, calories) {
+
         this.id = id,
             this.name = name,
             this.calories = calories;
@@ -9,9 +11,9 @@ const ItemCtrl = (function () {
     const data = {
 
         items: [
-            { id: 0, name: 'Steak', calories: 1200 },
-            { id: 1, name: 'Cookie', calories: 400 },
-            { id: 2, name: 'Eggs', calories: 300 },
+            // { id: 0, name: 'Steak', calories: 1200 },
+            // { id: 1, name: 'Cookie', calories: 400 },
+            // { id: 2, name: 'Eggs', calories: 300 },
         ],
 
         currentItem: null,
@@ -23,6 +25,29 @@ const ItemCtrl = (function () {
 
         getItems: function () {
             return data.items;
+        },
+        addItem: function (name, calories) {
+
+            let ID
+
+            if (data.items.length > 0) {
+
+                ID = data.items[data.items.length - 1].id + 1;
+
+            } else {
+
+                ID = 0;
+
+            }
+
+            calories = parseInt(calories);
+
+            newItem = new Item(ID, name, calories);
+
+            data.items.push(newItem);
+
+            return newItem;
+
         },
         logData: function () {
             return data;
@@ -37,13 +62,16 @@ const ItemCtrl = (function () {
 const UICtrl = (function () {
 
     const UISelectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
 
     return {
 
         populateItemList(items) {
-           
+
             let html = '';
 
             items.forEach(function (item) {
@@ -58,7 +86,48 @@ const UICtrl = (function () {
             document.querySelector(UISelectors.itemList).innerHTML = html;
 
 
+        },
+        getItemInput: function () {
+
+            return {
+                name: document.querySelector(UISelectors.itemNameInput).value,
+                calories: document.querySelector(UISelectors.itemCaloriesInput).value,
+            }
+
+        },
+        addListItem: function (item) {
+
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+
+            const li = document.createElement('li');
+
+            li.className = 'collection-item';
+
+            li.id = `item-${item.id}`;
+
+            li.innerHTML = `<strong>${item.name}: </strong><em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+            <i class="fas fa-edit"></i>
+            </a>`;
+
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
+
+
+
+        },
+        clearInput: function () {
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+
+        },
+        hideList: function () {
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+
+        },
+        getSelectors: function () {
+            return UISelectors;
         }
+
 
     }
 
@@ -69,13 +138,57 @@ const UICtrl = (function () {
 
 const App = (function (ItemCtrl, UICtrl) {
 
+    const loadEventListeners = function () {
+
+        const UISelectors = UICtrl.getSelectors();
+
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+
+
+    }
+
+    const itemAddSubmit = function (e) {
+
+        const input = UICtrl.getItemInput();
+
+        if (input.name !== '' && input.calories !== '') {
+
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+            UICtrl.addListItem(newItem);
+
+            UICtrl.clearInput();
+
+        }
+
+
+        e.preventDefault();
+    }
+
+
     return {
-        
+
         init: function () {
 
+            // Fetch items from data structure
             const items = ItemCtrl.getItems();
+            // Check if any items 
+            if (items.length === 0) {
+                UICtrl.hideList();
 
+            } else {
+                UICtrl.populateItemList(items);
+
+            }
+            // Populate list with items
             UICtrl.populateItemList(items);
+            // Load event listeners
+            loadEventListeners();
+
+
+
+
 
         }
     }
